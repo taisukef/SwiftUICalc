09233cf04de191d5528f12e45fec952f7e29cfcb
Taisuke Fukuno <fukuno@jig.jp>
2021-01-04 07:57:22 +0900

    Initial Commit

---
+++ Shared/SwiftUICalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalcApp.swift
+//  Shared
+//
+//  Created by Taisuke Fukuno on 2021/01/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
